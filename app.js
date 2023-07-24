83a6fa32041b9475a44f54ee3a4a6d288583aee9
ibjordan <94485638+ibjordan@users.noreply.github.com>
2023-07-24 16:06:25 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes/route')
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+
+app.listen(80, () => {
+    console.log('listening on host 3000')
+});
